6d90b63855b7c8c8f25d01352b6706d2497251b0
Oldemar <oldemego@gmail.com>
2024-02-03 17:56:21 +0000

    complete feature of authentication with email e password using cookie

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,14 @@
+﻿using web.Models;
+
+namespace web.Repositories;
+
+public static class UserRepository
+{
+    public static User? AuthenticateUser(string email, string password)
+    {
+        var users = new List<User>();
+        users.Add(new User {Id = 1, Email = "test1@email.com", Password = "batman", FullName = "Test 1"});
+        users.Add(new User {Id = 2, Email = "test2@email.com", Password = "batman", FullName = "Test 2"});
+        return users.Where(x => x.Email.ToLower() == email.ToLower() && x.Password == password).FirstOrDefault();
+    }
+}
---
+++ Services/CookieAuthentication.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using web.Models;
+using web.Repositories;
+
+namespace web.Services;
+
+public static class CookieAuthentication
+{
+    public static async Task<bool> Login(HttpContext httpContext, User user)
+    {
+        var authenticatedUser = UserRepository.AuthenticateUser(user.Email, user.Password);
+
+        if(authenticatedUser == null || string.IsNullOrEmpty(authenticatedUser.Email)){
+            return false;
+        }
+
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, authenticatedUser.Email),
+            new Claim(ClaimTypes.Role, authenticatedUser.Role),
+            new Claim("FullName", authenticatedUser.FullName)
+        };
+
+        var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+        await httpContext.SignInAsync(
+            CookieAuthenticationDefaults.AuthenticationScheme,
+            new ClaimsPrincipal(claimsIdentity),
+            new AuthenticationProperties{}
+        );
+        
+        return true;
+    }
+
+    public static async void Logout(HttpContext httpContext)
+    {
+        await httpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+    }
+}
